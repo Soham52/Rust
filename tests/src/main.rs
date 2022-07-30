880178c7e864e8cc60bdb52336bd8de4f2525c58
Soham Chakraborti <soham5chakraborti2006@gmail.com>
2022-07-30 15:53:18 +0530

    array test

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,4 +1,22 @@
+use std::io;
+
 fn main()
 {
-    println!("Hello, world!");
+    let a = [1, 2, 3, 4, 5];
+    let mut index = String::new();
+    
+    println!("Enter an index: ");
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line!");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Not an integer!");
+    
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
