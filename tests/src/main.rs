7c5b4a8753c20c3fd5778f1790cd0c71d6f953fd
Soham Chakraborti <soham5chakraborti2006@gmail.com>
2022-08-05 21:40:17 +0530

    Update main.rs

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,22 +1,13 @@
-use std::io;
-
 fn main()
 {
-    let a = [1, 2, 3, 4, 5];
-    let mut index = String::new();
-    
-    println!("Enter an index: ");
+    let s1 = String::from("hello");
 
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line!");
+    let len = calculate_length(&s1);
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Not an integer!");
-    
-    let element = a[index];
+    println!("The length of '{}' is {}.", s1, len);
+}
 
-    println!("The value of the element at index {index} is: {element}");
+fn calculate_length(s: &String) -> usize
+{
+    s.len()
 }
